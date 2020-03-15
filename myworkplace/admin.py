7c88b72cb3230605896e5398320e2c6dba4ed56e
pornchai <chaweewat.p@gmail.com>
2020-03-15 23:46:25 +0700

    add myworkplace app

--- myworkplace/admin.py
+++ myworkplace/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import employee
+from .models import employee, question
 
 # Register your models here.
+
 admin.site.register(employee)
+admin.site.register(question)
+
--- myworkplace/models.py
+++ myworkplace/models.py
@@ -5,6 +5,8 @@ class employee(models.Model):
     emplyee_name=models.CharField(max_length=300)
     employee_ID=models.IntegerField(default=999999)
     activity_text = models.TextField(max_length=300, default='000')  #
+    quarantined = models.BooleanField(default=False)
+    infected = models.BooleanField(default=False)
 
 
     def __str__(self):
@@ -13,3 +15,15 @@ class employee(models.Model):
 
     def update_activitiy(self):
         pass
+
+
+
+class question(models.Model):
+    question_text = models.CharField(max_length=200, blank=True)
+    answer_1 = models.CharField(max_length=200, blank=True)
+    answer_2 = models.CharField(max_length=200, blank=True)
+    answer_3 = models.CharField(max_length=200, blank=True)
+    correct_answer = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.question_text)
