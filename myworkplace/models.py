82114ce54af18cec03f3629e492d222cb79e39d6
pornchai <chaweewat.p@gmail.com>
2020-03-15 23:53:37 +0700

    add new migration file

--- myworkplace/models.py
+++ myworkplace/models.py
@@ -5,8 +5,8 @@ class employee(models.Model):
     emplyee_name=models.CharField(max_length=300)
     employee_ID=models.IntegerField(default=999999)
     activity_text = models.TextField(max_length=300, default='000')  #
-    quarantined = models.BooleanField(default=False)
-    infected = models.BooleanField(default=False)
+    quarantined = models.BooleanField(default=False, blank=True)
+    infected = models.BooleanField(default=False, blank=True)
 
 
     def __str__(self):
