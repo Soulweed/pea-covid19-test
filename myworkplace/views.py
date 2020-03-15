641bf45affc76a49260369aef892ad614b7328fe
pornchai <chaweewat.p@gmail.com>
2020-03-15 23:35:39 +0700

    add myworkplace app

--- myworkplace/views.py
+++ myworkplace/views.py
@@ -3,7 +3,6 @@ from .models import employee
 # Create your views here.
 def home(request):
     data1 = employee.objects.all()
-
     context = {'number_of_employee': len(data1)}
     print(context)
-    return render(request, 'applicant/home.html', context)
+    return render(request, 'myworkplace/home.html', context)
