9a045e7215a11854d162f7fadbb3a658ec1db03e
pornchai <chaweewat.p@gmail.com>
2020-03-16 11:59:51 +0700

    add serializers

--- myworkplace/urls.py
+++ myworkplace/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 # app_name = 'myworkplace'
+
+router = routers.DefaultRouter()
+router.register(r'questions', views.QuestionViewSet)
+
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('callback/', views.callback, name='callback'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
