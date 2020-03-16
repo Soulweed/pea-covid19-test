053412b644604a506c02058787f323e53c04d907
pornchai <chaweewat.p@gmail.com>
2020-03-16 19:27:19 +0700

    edit daily update form

--- myworkplace/urls.py
+++ myworkplace/urls.py
@@ -9,7 +9,7 @@ router.register(r'questions', views.QuestionViewSet)
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('daily_update/<int=id>/', views.daily_update, name='daily_update'),
+    path('daily_update/<id>/', views.daily_update, name='daily_update'),
     path('callback/', views.callback, name='callback'),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
