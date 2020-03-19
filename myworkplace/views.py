6929b326ec4f1e1169ecc1df9c977e12636472e1
pornchai <chaweewat.p@gmail.com>
2020-03-20 02:03:27 +0700

    edit line bot

--- myworkplace/views.py
+++ myworkplace/views.py
@@ -967,6 +967,7 @@ def handle_text_message(event):
                                                    }
                                                ]
                                            })
+                                           )
             else:
                 line_bot_api.reply_message(event.reply_token,
                                            TextSendMessage(
