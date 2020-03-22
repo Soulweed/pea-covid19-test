ee11ee476a091ec377de803421bb853d0a5f169f
pornchai <chaweewat.p@gmail.com>
2020-03-22 14:19:26 +0700

    edit line bot

--- line_bot/urls.py
+++ line_bot/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
-from .views import callback
+from .views import callback, send_question
 
+urlpatterns = [
+    path('callback/', callback, name='callback'),
+    path('send_question/', send_question, name='question'),
+]
 
-path('callback/', callback, name='callback'),
