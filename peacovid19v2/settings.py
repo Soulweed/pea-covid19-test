806bce5d2b9e6eed137850968ff4762fcd523029
pornchai <chaweewat.p@gmail.com>
2020-03-26 02:02:21 +0700

    xx

--- peacovid19v2/settings.py
+++ peacovid19v2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9j0su3m7x0cq)=)bfnl&vh_y@w*p&f=t8625pewl$^asm^buz&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
