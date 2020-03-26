6d1102e38b73667645d73d6737284c21a9399723
pornchai <chaweewat.p@gmail.com>
2020-03-26 15:13:33 +0700

    removeid

--- myworkplace/views.py
+++ myworkplace/views.py
@@ -562,11 +562,18 @@ def removeid(request):
     #     context['data'].append(line_id)
     # for item in employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b'):
     #     context['data'].append(item)
-    context['data1'].append(employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list(flat=True ))
-    employee.objects.filter(pk__in=employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list('id', flat=True )[1:]).delete()
-    context['data2'].append(employee.objects.filter(pk__in=employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list('id', flat=True )[1:]))
 
-    # context['data2'].append(employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list(flat=True ))
+
+
+
+    context['data1'].append(employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list(flat=True ))
+    # employee.objects.filter(pk__in=employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list('id', flat=True )[1:]).delete()
+    # context['data2'].append(employee.objects.filter(pk__in=employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list('id', flat=True )))
+
+
+
+
+    context['data2'].append(employee.objects.filter(employee_line_ID='U31b86969da2b249fcfb30164065d513b').values_list(flat=True ))
 
     connection.close()
     return render(request,'myworkplace/removeid.html', context)
